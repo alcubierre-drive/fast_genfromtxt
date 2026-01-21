944e22e2d7a897474b39cf1d50b07298ccb61998
alcubierre-drive <alcubierre-drive>
2026-01-21 17:27:04 +0100

    version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='fast_genfromtxt',
-   version='v0.0.1',
+   version='v0.1.0',
    description='fast version of numpy\'s genfromtxt/savetxt for floats',
    long_description_content_type = 'text/markdown',
    packages=['fast_genfromtxt'],
